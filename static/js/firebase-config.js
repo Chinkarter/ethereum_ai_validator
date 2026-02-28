7fa1237c621aa2925952ac545d1b388791ecc84e
Khloe Agent <khloe@khloe.ai>
2026-02-28 22:37:59 +0300

    V14: Update Firebase authDomain to custom subdomain

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -3,7 +3,7 @@
 // TODO: Replace with your actual Firebase project configuration
 const firebaseConfig = {
     apiKey: "AIzaSyByfYYb9g3yuV0Ilwv0kp6eX2on--54aF0",
-    authDomain: "eth1-c3f09.firebaseapp.com",
+    authDomain: "auth.ethaivalidators.site",
     projectId: "eth1-c3f09",
     storageBucket: "eth1-c3f09.firebasestorage.app",
     messagingSenderId: "891410320318",
